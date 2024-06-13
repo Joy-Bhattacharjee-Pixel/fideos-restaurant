eb8ecc795cfd86d5298b2f38fe6c44288433d36e
Anamika331 <127919428+Anamika331@users.noreply.github.com>
2024-06-13 12:46:57 +0530

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
