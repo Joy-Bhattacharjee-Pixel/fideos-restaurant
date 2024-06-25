1fb2c88a2345bd26441d241f2cd8cd09e9a7fe7f
missdipika15 <dipika.biswas@pixelconsultancy.in>
2024-06-25 17:31:05 +0530

    minor change

--- lib/presentations/food/add.dart
+++ lib/presentations/food/add.dart
@@ -16,70 +16,60 @@ class AddFoodScreen extends StatefulWidget {
 class _AddFoodScreenState extends State<AddFoodScreen> {
   // Importing controller
   final _controller = Get.put(FoodController());
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(),
-      body: Form(
-        key: _controller.foodAddFormKey,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              // Food name field
-              TextFieldService(
-                      controller: _controller.nameController, hint: "Name")
-                  .show(),
+        appBar: _appBar(),
+        body: Form(
+            key: _controller.foodAddFormKey,
+            child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(children: [
+                  // Food name field
+                  TextFieldService(
+                          controller: _controller.nameController, hint: "Name")
+                      .show(),
 
-              // Adding some space
-              const SizedBox(height: 10),
+                  // Adding some space
+                  const SizedBox(height: 10),
 
-              // Food description field
-              TextFieldService(
-                      controller: _controller.descriptionController,
-                      hint: "Decsription")
-                  .show(),
+                  // Food description field
+                  TextFieldService(
+                          controller: _controller.descriptionController,
+                          hint: "Decsription")
+                      .show(),
 
-              // Adding some space
-              const SizedBox(height: 10),
+                  // Adding some space
+                  const SizedBox(height: 10),
 
-              // Food price field
-              TextFieldService(
-                      controller: _controller.priceController, hint: "Price")
-                  .show(),
+                  // Food price field
+                  TextFieldService(
+                          controller: _controller.priceController,
+                          hint: "Price")
+                      .show(),
 
-              // Adding some space
-              const SizedBox(height: 10),
+                  // Adding some space
+                  const SizedBox(height: 10),
 
-              // Menu dropdwon
-              DropDownManager(
-                onChanged: () {},
-                value: "1",
-                hint: "Menu",
-                items: ["1", "2", "3"]
-              ).dropdown(),
+                  // Menu dropdwon
+                  DropDownManager(
+                      onChanged: () {},
+                      value: "1",
+                      hint: "Menu",
+                      items: ["1", "2", "3"]).dropdown(),
 
-              // Adding some space
-              const SizedBox(height: 20),
+                  // Adding some space
+                  const SizedBox(height: 20),
 
-              // Add button
-              ButtonManager(onPressed: () {}, text: "Add").elevated(),
-            ],
-          ),
-        ),
-      ),
-    );
+                  // Add button
+                  ButtonManager(onPressed: () {}, text: "Add").elevated()
+                ]))));
   }
 
   _appBar() => AppBar(
-        // Back button for navigation
-        leading: const Icon(Feather.arrow_left),
-        centerTitle: true,
-        title: const Text(
-          "Add Food",
-          style: TextStyle(fontSize: 18),
-        ),
-      );
+      // Back button for navigation
+      leading: const Icon(Feather.arrow_left),
+      centerTitle: true,
+      title: const Text("Add Food", style: TextStyle(fontSize: 18)));
 }
